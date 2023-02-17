c0d92b53f4dd28f5cf41b55b5a1ff4984dab4e70
Foxy20122012 <enriquemerlos49@gmail.com>
2023-02-17 00:50:55 -0600

    captura del onClick

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,12 +3,17 @@ import React, {useState, useEffect} from 'react';
 import Character from '../Personaje/Character';
 
 const NavPerson =()=>{
+  const[page,setPage] = useState(1)
   return(
-    <header>
-      <p>Page</p>
-      <div>
-        <button></button>
-        <button></button>
+    <header className=''>
+      <h2 className='text-white text-center '>Page:</h2>
+      <div className='text-center'>
+        <button className='text-white m-10 bg-black border border-y-8 rounded '
+        onClick={()=>console.log(`click`)}
+        >Anterior</button>
+        <button className='text-white m-10 bg-black border border-y-8 rounded'
+        onClick={()=>console.log(`click`)}
+        >Siguiente</button>
       </div>
     </header>
   )
@@ -36,6 +41,7 @@ const Card =()=>{
       
         <div className='bg-cyan-700 border  ' style={{backgroundImage: `url(https://i.pinimg.com/736x/82/81/0e/82810e099f9b1d7d6b43c96d19dfa4ef.jpg)`}}>
   <div className=" m-10 px-3" >
+    <NavPerson/>
   <h1 className="text-center  text-white text-4xl m-24"></h1>
 
   <div className='grid grid-cols-4 '>
--- src/components/Personaje/Character.jsx
+++ src/components/Personaje/Character.jsx
@@ -2,7 +2,7 @@
 
 const Character =({character})=>{
     return (
-        <div className="border m-5 border-y-4 border-x-4 rounded-t rounded bg-white"  style={{backgroundImage: `url(https://i.pinimg.com/originals/de/7e/15/de7e155b2c67618a96faee6172f5261e.jpg)`}}>
+        <div className="border m-10 border-y-4 border-x-4 rounded-t rounded bg-white"  style={{backgroundImage: `url(https://i.pinimg.com/originals/de/7e/15/de7e155b2c67618a96faee6172f5261e.jpg)`}}>
           <h2 className='text-center text-white m-5 text-2xl'>{character.name}</h2>
           <div className="border rounded-r">
           <h4 className="text-white text-center text-base  ">{character.status}</h4>
